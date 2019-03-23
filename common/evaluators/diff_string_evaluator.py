d92bca35d98d2c0aa3003bff7ac8fc66c1cbf960
Achyudh Ram <achyudhk@gmail.com>
2019-03-23 12:18:07 -0400

    Clean up KimCNN code with diff_string_trainer

--- common/evaluators/diff_string_evaluator.py
+++ common/evaluators/diff_string_evaluator.py
@@ -11,14 +11,13 @@ class DiffStringEvaluator(Evaluator):
     def __init__(self, dataset_cls, model, embedding, data_loader, batch_size, device, keep_results=False):
         super().__init__(dataset_cls, model, embedding, data_loader, batch_size, device, keep_results)
         self.ignore_lengths = False
-        self.single_label = False
+        self.is_multilabel = False
 
     def get_scores(self):
         self.model.eval()
         self.data_loader.init_epoch()
         total_loss = 0
 
-        # Temp Ave
         if hasattr(self.model, 'beta_ema') and self.model.beta_ema > 0:
             old_params = self.model.get_params()
             self.model.load_ema_params()
@@ -36,15 +35,15 @@ class DiffStringEvaluator(Evaluator):
                 else:
                     scores = self.model(batch.text[0], lengths=batch.text[1])
 
-            if self.single_label:
-                predicted_labels.extend(torch.argmax(scores, dim=1).cpu().detach().numpy())
-                target_labels.extend(torch.argmax(batch.label, dim=1).cpu().detach().numpy())
-                total_loss += F.cross_entropy(scores, torch.argmax(batch.label, dim=1), size_average=False).item()
-            else:
+            if self.is_multilabel:
                 scores_rounded = F.sigmoid(scores).round().long()
                 predicted_labels.extend(scores_rounded.cpu().detach().numpy())
                 target_labels.extend(batch.label.cpu().detach().numpy())
                 total_loss += F.binary_cross_entropy_with_logits(scores, batch.label.float(), size_average=False).item()
+            else:
+                predicted_labels.extend(torch.argmax(scores, dim=1).cpu().detach().numpy())
+                target_labels.extend(torch.argmax(batch.label, dim=1).cpu().detach().numpy())
+                total_loss += F.cross_entropy(scores, torch.argmax(batch.label, dim=1), size_average=False).item()
 
             if hasattr(self.model, 'TAR') and self.model.TAR:  # TAR condition
                 total_loss += (rnn_outs[1:] - rnn_outs[:-1]).pow(2).mean()
@@ -58,7 +57,6 @@ class DiffStringEvaluator(Evaluator):
         f1 = metrics.f1_score(target_labels, predicted_labels, average=None)[0]
         avg_loss = total_loss / len(self.data_loader.dataset.examples)
 
-        # Temp Ave
         if hasattr(self.model, 'beta_ema') and self.model.beta_ema > 0:
             self.model.load_params(old_params)
 
